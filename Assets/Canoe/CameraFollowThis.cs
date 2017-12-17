425e8f33dce334d216ece05aa8068bd0e6f732b5
Shachar Oz <shachar.oz@gmail.com>
2017-12-17 15:38:39 +0200

    init commit
    canoe driving in river following keyboards

---
+++ Assets/Canoe/CameraFollowThis.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowThis : MonoBehaviour {
+
+	public Transform NestUnderThisTransform;
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = NestUnderThisTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Canoe/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    public float SPEED = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * SPEED);
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * 1);
+        }
+
+    }
+}
