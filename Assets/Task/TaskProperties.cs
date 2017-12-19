a4045b07f94285b2e7d0563ca9969f765e9533a4
Shachar Oz <shachar.oz@gmail.com>
2017-12-19 17:30:27 +0200

    graphics skeybox and stimulation properties

--- Assets/Task/TaskProperties.cs
+++ Assets/Task/TaskProperties.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class TaskProperties : MonoBehaviour {
 
+	public Mechanism mechanism;
+
 	//cue
 	public float cueStartTime = 2;
 	public float cueLengthTime = 2;
 
-	public float stimualteStartTime = 4;
+	//stimulation
+	public List<Stimulus> Stimuli;
 
 
 
+
+
+
+	public enum Mechanism {
+		SequenceMatching,
+		RepeatSequence,
+		CountTimes,
+		IdentifyStronger,
+
+	}
+
+}
+
+[System.Serializable]
+public class Stimulus{
+	public string description;
+	public float timeForStimulus;
+	public float timeGapAfter;
 }
---
+++ Assets/Task/UITaskController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UITaskController : MonoBehaviour {
+
+
+	public UnityEvent CueStart;
+	public UnityEvent StimulationStart;
+	public UnityEvent InputStart;
+	public UnityEvent TaskEnd;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
